8e2b021ce6711851ee925720a03970e45ebf6226
rkolen <rkolen2007@gmail.com>
2018-02-08 09:54:46 +0100

    [w3-007] fixed all comments on blog shown on load

--- Reader/Scripts/reader.js
+++ Reader/Scripts/reader.js
@@ -1,9 +1,12 @@
 var currentContent = "";
 var xhr = new XMLHttpRequest();
 
+
+
+
+
  window.onload = function() {
 
-		var blogcategories = document.getElementById("categories").value;
     	var blogScreen = document.getElementById("blogpost");
     	xhr.open("GET", "reader.php?", false);
 		xhr.send();
@@ -13,18 +16,18 @@ var xhr = new XMLHttpRequest();
 		jQuery('#blogpost').html('');
 		for (var i = 0 ; i < returnblog.length ; i++) {
 			var b = returnblog[i];
-			document.getElementById("blogpost").innerHTML += "<div class='new-post'>"+ b[2] + "</div>" + "<br>";
-   			
-  
-   			
+
+			document.getElementById("blogpost").innerHTML += "<div class='new-post'>"+ b[1] + "</div>" + "<br>";
+			
 		}
+
 }
 
 function getBlogpost () {
 
     	var blogcategories = document.getElementById("categories").value;
     	var blogScreen = document.getElementById("blogpost");
-    	xhr.open("GET", "reader.php?", false);
+    	xhr.open("GET", "reader2.php?", false);
 		xhr.send();
 		returnblog = JSON.parse(xhr.response);
 		returnblog.reverse();
@@ -38,7 +41,39 @@ function getBlogpost () {
 		
    				document.getElementById("blogpost").innerHTML += "<div class='new-post'>"+ b[2] + "</div>" + "<br>";
    			
-  
    			}
 		}
 }
+
+/*function commentBlogpost(e){
+	if(e.target.classList.contains('commentbutton')){
+		commentAddFormDiv.style.display = "block";
+}*/
+
+
+
+
+
+
+
+
+
+
+
+/*function editEntry(e){
+		// Remove old entry from the addressbook
+		if(e.target.classList.contains('editbutton')){
+			var editcontact = e.target.getAttribute('contactedit');
+			localStorage['addbook'] = JSON.stringify(addressBook);
+			document.getElementById("fullname").value = addressBook[e.target.getAttribute("contactedit")].fullname;
+			document.getElementById("phone").value = addressBook[e.target.getAttribute("contactedit")].phone;
+			document.getElementById("address").value = addressBook[e.target.getAttribute("contactedit")].address;
+			document.getElementById("email").value = addressBook[e.target.getAttribute("contactedit")].fullname;
+			// adds new entry to the addressbook has to be completely filled
+			contactAddFormDiv.style.display = "block";
+			// to make the cancel button dissappear for editing it causes errors otherwise
+			Cancel.style.display = "none";
+			addressBook.splice(editcontact,1);
+			document.getElementById("fullname").innerHTML = fullname.value;
+		}
+	}*/
